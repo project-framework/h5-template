5a816c6a73b9b6b30740a0a8e504f48354e2e3e6
Zhangyao719 <1258858403@qq.com>
2024-05-29 09:44:26 +0800

    feat: vant 按需导入

--- types/components.d.ts
+++ types/components.d.ts
@@ -7,7 +7,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    VanButton: typeof import('vant/es')['Button']
+    HelloWorld: typeof import('./../src/components/HelloWorld.vue')['default']
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from '@vant/auto-import-resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
     return {
-        plugins: [vue()],
+        plugins: [
+            vue(),
+            AutoImport({
+                dts: path.resolve(__dirname, 'types', 'auto-imports.d.ts'),
+                resolvers: [VantResolver()],
+            }),
+            Components({
+                dts: path.resolve(__dirname, 'types', 'components.d.ts'),
+                resolvers: [VantResolver()],
+            }),
+        ],
     };
 });
